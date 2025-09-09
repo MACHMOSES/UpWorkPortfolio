619bf5dd73e3a4316d473107a2c9a0b77303372e
MACHMOSES <machariamoses39@gmail.com>
2025-09-09 12:40:55 +0300

    css to optimize phone screens and tab screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,17 +2,10 @@ import {Link} from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
-    const addStyle = (routelink) => {
-        const element = document.getElementsByClassName(routelink);
-        if(element){
-            element.style.color = 'aliceblue'
-            element.style.fontWeight = 'bold'
-            element.style.fontSize = '20px'
-        }
-    }
+    
     return (
             <nav className="navbar">
-                <h3>machmoses</h3>
+                <h3 className='profileLogo'>machmoses</h3>
                 <div className='navlink'>
                     <Link to="/" className='routelink'>Home</Link>{" "}
                     <Link to="/about" className='routelink'>About</Link>{" "}
